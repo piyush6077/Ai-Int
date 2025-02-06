6141fc5c3424259633ff02eb0f75a48e73ca20eb
Piyush patil <098piyushpatil@gmail.com>
2025-02-06 11:11:16 +0530

    Implement Resend integration for sending verification emails and establish database connection

---
+++ emails/VerificationEmail.tsx
@@ -0,0 +1,20 @@
+import {
+    Html, 
+    Head, 
+    Font,
+    Preview, 
+    Heading, 
+    Row,
+    Section, 
+    Text, 
+    Button
+} from "@react-email/components";
+
+interface VerificationEmailProps {
+    username:string,
+    otp:string
+}
+
+export default function VerificationEmail ({username, otp}: VerificationEmailProps){
+    return ()
+}
