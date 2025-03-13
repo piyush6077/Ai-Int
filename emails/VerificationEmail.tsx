0196bb54d096370c8c2fe325bae9bca6bca5100b
Piyush patil <098piyushpatil@gmail.com>
2025-03-13 23:29:38 +0530

    Add verification email template and implement email sending functionality; remove unused schemas

--- emails/VerificationEmail.tsx
+++ emails/VerificationEmail.tsx
@@ -16,5 +16,41 @@ interface VerificationEmailProps {
 }
 
 export default function VerificationEmail ({username, otp}: VerificationEmailProps){
-    return ()
+    return (
+        <Html lang="" dir="ltr">
+            <Head>
+                <title>Verififcaton Code</title>
+                <Font 
+                    fontFamily="Roboto"
+                    fallbackFontFamily="Verdana"
+                    webFont={{
+                        url:'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92FrlMu4mxKkTU1Kg.woff2',
+                        format: "woff2"
+                    }}
+                    fontWeight={400}
+                    fontStyle="normal"
+                />
+                <Preview>Here&apos;s your verification code: {otp}</Preview>
+                <Section>
+                    <Row>
+                        <Heading as="h2">Hello {username}</Heading>
+                    </Row>
+                    <Row>
+                        <Text>
+                            Thanks you for registering , Please use the following verification 
+                            code to complete your registration 
+                        </Text>
+                    </Row>
+                    <Row>
+                        <Text>{otp}</Text>
+                    </Row>
+                    <Row>
+                        <Text>
+                            If you did not request this code , Ignore the Email
+                        </Text>
+                    </Row>
+                </Section>
+            </Head>
+        </Html>
+    )
 }
--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -7,15 +7,15 @@ type ConnectionObject = {
 const connection: ConnectionObject = {}
 
 async function dbConnect(): Promise<void> {
+    //Checking if connection is available 
     if(connection.isConnected){
         console.log("Already connected to database")
+        return
     }
 
     try {
         const db = await mongoose.connect(process.env.MONGOURI || '',{})
-        
         connection.isConnected = db.connections[0].readyState
-        
         console.log("DB connected Successfully")
     } catch (error) {
         console.log("Database connection failed", error)
